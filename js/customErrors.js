7f814ddced764e194a89fbc59361c8f09462fbfa
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-05-22 22:04:34 -0300

    cambios en registro y ás

---
+++ js/customErrors.js
@@ -0,0 +1,33 @@
+export const tiposError = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "tooShort",
+    "customError",
+  ];
+  export const mensajes = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío.",
+      tooShort: "Por favor, ingrese un nombre válido.",
+    },
+    apellido: {
+      valueMissing: "El campo nombre no puede estar vacío.",
+      tooShort: "Por favor, ingrese un nombre válido.",
+    },
+    email: {
+      valueMissing: "El campo email no puede estar vacío.",
+      typeMismatch: "Por favor, ingrese un email válido.",
+      tooShort: "Por favor, ingrese un e-mail válido.",
+    },
+    telefono: {
+      valueMissing: "El campo telefono no puede estar vacío.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+    contraseña: {
+      valueMissing: "El campo contraseña no puede estar vacío.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+
+
+  };
+  
