ddcb134daed2756511111e0cf2043565f5f8d884
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-07-03 20:16:59 -0300

    free sql config

--- db/mysql.config.js
+++ db/mysql.config.js
@@ -1,7 +1,6 @@
 export const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'node24132'
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB
 }
