5439639085096c77ff5e7476c7c49cd55d65659c
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-07-06 00:17:18 -0300

    coreccion

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,8 +1,9 @@
 const config = {
-    host: process.env.HOST_DB,
-    user: process.env.USER_DB,
-    password: process.env.PASS_DB,
-    database: process.env.DB
+    host: process.env.HOST_DB, // Host: sql3.freesqldatabase.com
+    user: process.env.USER_DB, // Database user: sql3717791
+    password: process.env.PASS_DB, // Database password: YIpfUD8Xfb
+    database: process.env.DB  // Database name: sql3717791
+    // Port number: 3306
 
     // host: 'localhost',
     // user: 'root',
